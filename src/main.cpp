6763a9ec297cf2cc6d53c4d209590d1beb47367f
Maschell <Maschell@gmx.de>
2022-01-30 17:22:14 +0100

    Remove unneeded code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include <wups.h>
-#include <whb/log.h>
-#include <whb/log_udp.h>
-#include "utils/utils.h"
 
 WUPS_PLUGIN_NAME("drc_region_free_plugin");
 WUPS_PLUGIN_DESCRIPTION("Allows the usage of gamepads from every region");
 WUPS_PLUGIN_VERSION("0.1");
 WUPS_PLUGIN_AUTHOR("Maschell");
 WUPS_PLUGIN_LICENSE("GPL");
-
-/* Entry point */
-ON_APPLICATION_START() {    
-    WHBLogUdpInit();
-}
 
 DECL_FUNCTION(uint32_t, CCRSysDRCFWUpdate, uint32_t u1) {
     return 0;
